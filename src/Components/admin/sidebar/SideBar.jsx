7ca9cadd0e86b9e5098b06c0789f6c83a1341247
SanjeevThalod <thalodsanjeev@gmail.com>
2024-11-08 18:07:50 +0530

    sidebar-responsive

--- src/Components/admin/sidebar/SideBar.jsx
+++ src/Components/admin/sidebar/SideBar.jsx
@@ -1,27 +1,40 @@
 import { useEffect, useState } from "react";
 import { AiFillFileText } from "react-icons/ai";
-import { FaChartBar, FaChartLine, FaChartPie, FaGamepad, FaStopwatch } from "react-icons/fa";
+import {
+  FaChartBar,
+  FaChartLine,
+  FaChartPie,
+  FaGamepad,
+  FaStopwatch,
+} from "react-icons/fa";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { IoIosPeople } from "react-icons/io";
-import { RiApps2AddFill, RiCoupon3Fill, RiShoppingBag3Fill, RiDashboardFill } from "react-icons/ri";
+import {
+  RiApps2AddFill,
+  RiCoupon3Fill,
+  RiShoppingBag3Fill,
+  RiDashboardFill,
+} from "react-icons/ri";
 import { Link, useLocation } from "react-router-dom";
 import "./SideBar.css";
 
-const AdminSidebar = ({ isOpen, toggleSidebar }) => {
+const AdminSidebar = () => {
   const location = useLocation();
-  const [phoneActive, setPhoneActive] = useState(window.innerWidth < 1100);
+  const [isOpen, setIsOpen] = useState(true);
+  const [phoneActive, setPhoneActive] = useState(window.innerWidth < 900);
 
-  const resizeHandler = () => {
-    setPhoneActive(window.innerWidth < 1100);
-  };
+  const toggleSidebar = () => setIsOpen(!isOpen);
 
   useEffect(() => {
-    window.addEventListener("resize", resizeHandler);
-    return () => {
-      window.removeEventListener("resize", resizeHandler);
-    };
-  }, []);
+    const resizeHandler = () => setPhoneActive(window.innerWidth < 900);
 
+    resizeHandler();
+    
+    window.addEventListener("resize", resizeHandler);
+    return () => window.removeEventListener("resize", resizeHandler);
+  }, []);
+  console.log(phoneActive);
+  
   return (
     <>
       {phoneActive && (
@@ -29,10 +42,7 @@ const AdminSidebar = ({ isOpen, toggleSidebar }) => {
           <HiMenuAlt4 />
         </button>
       )}
-
-      <aside
-        className={`sidebar ${phoneActive && !isOpen ? "collapsed" : ""}`}
-      >
+      <aside className={`sidebar ${isOpen ? "" : "collapsed"}`}>
         <DivOne location={location} />
         <DivTwo location={location} />
         <DivThree location={location} />
@@ -50,11 +60,36 @@ const DivOne = ({ location }) => (
   <div>
     <h5>Dashboard</h5>
     <ul>
-      <Li url="/admin/dashboard" text="Dashboard" Icon={RiDashboardFill} location={location} />
-      <Li url="/admin/product" text="Product" Icon={RiShoppingBag3Fill} location={location} />
-      <Li url="/admin/categories" text="Category" Icon={RiApps2AddFill} location={location} />
-      <Li url="/admin/customer" text="Customer" Icon={IoIosPeople} location={location} />
-      <Li url="/admin/transaction" text="Transaction" Icon={AiFillFileText} location={location} />
+      <Li
+        url="/admin/dashboard"
+        text="Dashboard"
+        Icon={RiDashboardFill}
+        location={location}
+      />
+      <Li
+        url="/admin/product"
+        text="Product"
+        Icon={RiShoppingBag3Fill}
+        location={location}
+      />
+      <Li
+        url="/admin/categories"
+        text="Category"
+        Icon={RiApps2AddFill}
+        location={location}
+      />
+      <Li
+        url="/admin/customer"
+        text="Customer"
+        Icon={IoIosPeople}
+        location={location}
+      />
+      <Li
+        url="/admin/transaction"
+        text="Transaction"
+        Icon={AiFillFileText}
+        location={location}
+      />
     </ul>
   </div>
 );
@@ -63,7 +98,12 @@ const DivTwo = ({ location }) => (
   <div>
     <h5>Charts</h5>
     <ul>
-      <Li url="/admin/charts" text="All Charts" Icon={FaChartBar} location={location} />
+      <Li
+        url="/admin/charts"
+        text="All Charts"
+        Icon={FaChartBar}
+        location={location}
+      />
       {/* <Li url="/admin/chart/pie" text="Pie" Icon={FaChartPie} location={location} />
       <Li url="/admin/chart/line" text="Line" Icon={FaChartLine} location={location} /> */}
     </ul>
@@ -74,9 +114,24 @@ const DivThree = ({ location }) => (
   <div>
     <h5>Apps</h5>
     <ul>
-      <Li url="/admin/app/stopwatch" text="Stopwatch" Icon={FaStopwatch} location={location} />
-      <Li url="/admin/coupons" text="Coupons" Icon={RiCoupon3Fill} location={location}/>
-      <Li url="/admin/app/toss" text="Toss" Icon={FaGamepad} location={location} />
+      <Li
+        url="/admin/app/stopwatch"
+        text="Stopwatch"
+        Icon={FaStopwatch}
+        location={location}
+      />
+      <Li
+        url="/admin/coupons"
+        text="Coupons"
+        Icon={RiCoupon3Fill}
+        location={location}
+      />
+      <Li
+        url="/admin/app/toss"
+        text="Toss"
+        Icon={FaGamepad}
+        location={location}
+      />
     </ul>
   </div>
 );
